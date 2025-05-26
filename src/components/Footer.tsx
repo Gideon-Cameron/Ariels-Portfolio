36ce8c155d2d3e7e53ee3cb784162cdd804b1be7
Gideon Cameron <gideonwork20@gmail.com>
2025-05-26 02:57:23 -0700

    Updated Footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,15 +25,9 @@ const Footer = () => {
             href="mailto:gideonwork20@gmail.com"
             className="hover:text-[#007acc] dark:hover:text-[#64ffda] transition"
           >
-            gideonwork20@gmail.com
+            arielgermond@gmail.com
           </a>{" "}
-          |{" "}
-          <a
-            href="tel:+251980277582"
-            className="hover:text-[#007acc] dark:hover:text-[#64ffda] transition"
-          >
-            +251 980 277 582
-          </a>
+          
         </p>
       </div>
     </motion.footer>
